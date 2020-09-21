ebad9e797fd26e019e6eb8df08098e6d645f2e57
Idriss Kone <idrisskone@Idrisss-MacBook-Pro.local>
2020-09-21 15:36:09 -0400

    add colorhelper to genrate colors

--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+import seedColors from "./seedColors";
+
 export default class Palette extends Component {
   render() {
     const colorBoxes = this.props.colors.map(color => (
       <ColorBox background={color.color} name={color.name} />
     ));
+
     return (
       <div className="Palette">
         {/* Navbar goes here */}
