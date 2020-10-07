e8a83640ebf2510752ac1c224d82a59a736a0f0d
Idriss Kone <idrisskone@Idrisss-MacBook-Pro.local>
2020-10-07 16:30:32 -0400

    add new palette form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { generatePalette } from "./colorHelpers";
 import { Route, Switch } from "react-router-dom";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 
 export default class App extends Component {
   findPalette = id => {
@@ -15,6 +16,7 @@ export default class App extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/"
